f6aa19c0a25f5f61f15890d19e002a728a70d4de
tejpatel1302 <tgp02tp@gmail.com>
2024-08-04 18:14:11 +0530

    Added Basic User Model

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ const app = express();
 
 app.use('/api', apiRoutes);
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
 });
